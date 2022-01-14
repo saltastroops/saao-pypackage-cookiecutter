d4632f34db7e3f6ea2ddb8d791234272c47fcfe0
Christian Hettlage <hettlage@saao.ac.za>
2022-01-14 23:09:33 +0200

    Tell the user what to do after creating a new project

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,21 @@
+print()
+print("Next steps")
+print("----------")
+print()
+print("* cd into {{ cookiecutter.package_slug }}")
+print()
+print("* Create a virtual environment called .venv and activate it")
+print()
+print("* Install the packages needed for development:\n"
+      "  pip install -r requirements-dev")
+print()
+print("* Check the Python versions in the following files:\n"
+      "  - setup.cfg\n"
+      "  - pyproject.toml\n"
+      "  - GitHub action files in .github/workflows/")
+print()
+print("* Put the project under version control:\n"
+      "  git init")
+print()
+print("* Make sure tox is installed on your machine")
+print()
